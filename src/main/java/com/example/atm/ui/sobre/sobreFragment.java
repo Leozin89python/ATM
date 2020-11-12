4bb94a81d75890b868d40fa016688ff1b3e94544
Leozin89Python <leozin069@gmail.com>
2020-11-11 21:57:12 -0300

    ATM v1.4

--- src/main/java/com/example/atm/ui/sobre/sobreFragment.java
+++ src/main/java/com/example/atm/ui/sobre/sobreFragment.java
@@ -11,6 +11,8 @@ import android.view.ViewGroup;
 
 import com.example.atm.R;
 
+import mehdi.sakout.aboutpage.AboutPage;
+
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -25,8 +27,21 @@ public class sobreFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sobre, container, false);
-    }
 
+        String descricao = "A ATM consultoria tem como missão apoiar organizações" +
+                            "que desejam alcançar através da excelência em gestão e " +
+                             "da busca pela qualidade";
+
+    return new AboutPage(getActivity(  ))
+                .setImage( R.drawable.logo )
+                .setDescription( descricao )
+                .addGroup("entre em contato")
+                .addEmail("leo@gmail.com","envie um e-mail")
+                .addWebsite("www.leo.com","acesse nosso site.")
+                .addGroup("redes sociais")
+                .addFacebook("Leo Sousa","facebook")
+                .addInstagram("Leozin","instagram")
+                .addGitHub("https://github.com/medyo/android-about-page","github")
+                .create();
+    }
 }
